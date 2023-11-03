cdbf6e7996ad8aa7e697bbbc70b7f2661e8db364
Carlos Motiño <carlos.motino@alcon.com>
2023-11-03 11:38:40 -0600

    ejemplo de useForm.

--- react-app/src/03-forms/hooks/useForm.ts
+++ react-app/src/03-forms/hooks/useForm.ts
@@ -19,12 +19,23 @@ export const useForm = <T>( initState: T ) => {
         setFormData(prev =>({
             ...prev,
             [event.target.name]: event.target.value
-        }));
+        }));        
     }
+
+    const resetForm = () => {
+      setFormData({...initState});
+    }
+
+    const isValidEmail = ( email: string ) => {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+  }
 
   return {
     ...formData,
     formData,
-    onChange
+    onChange,
+    resetForm,
+    isValidEmail
   }
 }
--- react-app/src/03-forms/pages/RegisterPage.tsx
+++ react-app/src/03-forms/pages/RegisterPage.tsx
@@ -4,7 +4,16 @@ import { useForm } from '../hooks/useForm'
 
 export const RegisterPage = () => {
 
-    const { formData, onChange, name,email,password1,password2 } = useForm({
+    const { 
+        formData, 
+        onChange, 
+        name,
+        email,
+        password1,
+        password2,
+        resetForm,
+        isValidEmail
+    } = useForm({
         name:'',
         email:'',
         password1:'',
@@ -15,8 +24,6 @@ export const RegisterPage = () => {
         event.preventDefault();
         console.log(formData)
     }
-
-
 
   return (
     <div>
@@ -29,14 +36,20 @@ export const RegisterPage = () => {
                 value={name}
                 onChange={onChange}
                 placeholder='Name'
+                className={ `${ name.trim().length <= 0 && 'has-error' }` }
              />
+             { name.trim().length <= 0 && <span>Este campo es necesario</span>}
+
             <input 
                 type='email'
                 name="email"
                 value={email}
                 onChange={onChange}
                 placeholder='Email'
+                className={ `${!isValidEmail( email ) && 'has-Error'}` }
              />
+             { !isValidEmail( email ) && <span>Email no es válido</span> }
+
             <input 
                 type='password'
                 name="password1" 
@@ -44,6 +57,9 @@ export const RegisterPage = () => {
                 onChange={onChange}
                 placeholder='Password'
              />
+             { password1.trim().length <= 0 && <span>Este campo es necesario</span>}
+             { password1.trim().length < 6 && password1.trim().length > 0 && <span>La contraseña tiene que tener mas de 6 caracteres.</span>}
+
             <input 
                 type='password'
                 name="password2" 
@@ -51,9 +67,14 @@ export const RegisterPage = () => {
                 onChange={onChange}
                 placeholder='Repeat Password'
              />
+            { password2.trim().length <= 0 && <span>Este campo es necesario</span>}
+            { password2.trim().length > 0 && password1 !== password2 && <span>Las contraseñas no coinciden.</span>}
 
             <button type="submit">
                 Create
+            </button>
+            <button type='button' onClick={resetForm}>
+                Reset
             </button>
         </form>
     </div>
